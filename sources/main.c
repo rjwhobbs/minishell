617a82cf42603172debd686eb5e0f60347643a38
Roger HOBBS <rhobbs@c4r6s4.wethinkcode.co.za>
2019-09-06 16:48:23 +0200

    No one likes conflicts

--- sources/main.c
+++ sources/main.c
@@ -5,8 +5,9 @@ int		main(int ac, char *av[], char *env[])
 	char *env_var;
 
 	(void)ac;
-	(void)av;
-	env_var = param_search(env, "PATH", NULL, SEARCH_VAL);
+	//(void)av;
+	env_var = param_search(env, "PATH", av[1], SEARCH_ON);
+	ft_strjoin(env_var, av[1]);
 	ft_putendl(env_var);
 	return (0);
 }
