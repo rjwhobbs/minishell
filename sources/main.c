60a5ae7b93811f44f98f1b4ba96015c0b1f063a2
Roger HOBBS <rhobbs@c4r6s4.wethinkcode.co.za>
2019-09-10 16:31:51 +0200

    Basic cd test

--- sources/main.c
+++ sources/main.c
@@ -13,12 +13,12 @@ int		main(int ac, char *av[], char *env[])
 {
 	(void)ac;
 	(void)av;
-	
+	int		i;
+	i = 1;
 	g_environ_vars = ft_strarrdup(env);
 	print_env(g_environ_vars);
-	//ft_setenv(av[1], av[2], ft_strlen(av[1]), ft_strlen(av[2]));
 	ft_putendl("........................");
-	ft_unsetenv(av[1]);
+	ft_cd(av[1]);
 	print_env(g_environ_vars);
 	ft_strstrdel(&g_environ_vars);
 	return (0);
