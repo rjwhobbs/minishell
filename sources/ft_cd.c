19eddc1dbb96ff2c05a9984889743ec7565308c9
Roger HOBBS <rhobbs@c4r6s4.wethinkcode.co.za>
2019-09-11 10:29:44 +0200

    testing

--- sources/ft_cd.c
+++ sources/ft_cd.c
@@ -3,10 +3,12 @@
 int		ft_cd(char *path)
 {
 	char	oldpwd[PATH_MAX];
+	char	pwd[PATH_MAX];
 
 	getcwd(oldpwd, PATH_MAX);
 	ft_setenv("OLDPWD", oldpwd, ft_strlen("OLDPWD"), ft_strlen(oldpwd));
 	chdir(path);
-	ft_setenv("PWD", path, ft_strlen("PWD"), ft_strlen(path));
+	getcwd(pwd, PATH_MAX);
+	ft_setenv("PWD", pwd, ft_strlen("PWD"), ft_strlen(pwd));
 	return (1);
 }
