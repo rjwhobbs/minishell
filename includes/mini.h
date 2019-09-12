601f38c83006f7323d0c1c171a021069be3eff89
Roger HOBBS <rhobbs@c4r6s4.wethinkcode.co.za>
2019-09-12 10:52:16 +0200

    I-wanna-be-clever

--- includes/mini.h
+++ includes/mini.h
@@ -20,5 +20,6 @@ char		**ft_strarrrealloc(char **arr, char *str);
 void		ft_setenv(char *var_name, char *value, size_t varlen, size_t vallen);
 void		ft_unsetenv(char *varname);
 int			ft_cd(char *path);
+char		**parser(char *input);
 
 #endif
