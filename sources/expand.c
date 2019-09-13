7f27b3baa8d41b6edc8610b0da36db65d1e0d6c6
Roger HOBBS <rhobbs@c4r6s4.wethinkcode.co.za>
2019-09-13 12:06:00 +0200

    Fixed some of the issues with expand, needs more testing though

--- sources/expand.c
+++ sources/expand.c
@@ -3,10 +3,19 @@
 static void injector(char *value, char **str, char *ptr)
 {
 	char new[ARG_MAX];
+	char	*wordend;
 
 	ft_bzero(new, ARG_MAX);
 	ft_memcpy(new, *str, ptr - *str);
-	ft_strcat(new, value);
+	if (value)
+		ft_strcat(new, value);
+	else
+	{
+		if (!(wordend = ft_strchr(ptr, ' ')))
+			wordend = ft_strchr(ptr, '\0');
+		// if ((wordend - ptr) == 1)
+		// 	ft_strcat(new,"$");		
+	}
 	if ((ptr = ft_strchr(ptr, ' ')))
 		ft_strcat(new, ptr);
 	ft_strdel(str);
@@ -29,14 +38,18 @@ static void expand_checker(char **str)
 		{
 			if (!(word = ft_strchr(ptr, ' ')))
 				word = ft_strchr(ptr, '\0');
+			j = word - ptr;
+			// ft_putnbr(j);
+			// ft_nl();
 			temp = ft_strsub(ptr, 1, word - (ptr + 1));
+			// ft_putstr(temp);
+			// ft_putendl("X");
 			value = param_search(g_environ_vars, temp, NULL, SEARCH_VAL);
-			if (value)
+			//if (value)
 				injector(value, str, ptr);
-			else
-				ft_putendl("Can't find string!!!!");
+			//else
+			//	ft_putendl("Can't find string!!!!");
 			free(temp);
-			j = word - *str;
 		}
 		else
 			j++;
